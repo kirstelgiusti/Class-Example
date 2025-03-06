3e05f4eedc7d263e747780704b5ff1079716eb95
kirstelgiusti <147386839+kirstelgiusti@users.noreply.github.com>
2025-03-06 16:00:27 +0100

    Player respawning and health text

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,8 +1,17 @@
+using System.Security.Cryptography;
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public int points = 5;
+    public Vector3 respawnPosition;
+    public TMP_Text healthText;
+
+    private void Start()
+    {
+        respawnPosition = transform.position;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -11,12 +20,27 @@ public class Health : MonoBehaviour
             Damage(1);
         }
 
+        if (other.CompareTag("Checkpoint"))
+        {
+            // we can set the position based on the checkpoint so that this acts as a guaranteed safe spot 
+            respawnPosition = other.transform.position;
+            respawnPosition.y = transform.position.y;
+        }
+
     }
 
     //to remove some health points 
     private void Damage(int value)
     {
         points = points - value;
+        healthText.text = $"<b>Health:</b> {points}";
+        
+        //move the player to the start
+        transform.position = respawnPosition; 
+        
+        //reset points to 5
+        // points = 5; 
+        
         //if health is 0 or less, destroy player
         if(points < 1)
         {
@@ -26,7 +50,4 @@ public class Health : MonoBehaviour
 
 }
 
-//homework:
-//do not destroy
-// move the player to the start
-// reset points to 5 
+
--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerCollection : MonoBehaviour 
 {
     
+    // add a scoring system here (HW) 
+    
     private void OnTriggerEnter(Collider other)
     {
         // only destroy if its a collectible
